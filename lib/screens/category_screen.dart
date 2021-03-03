0d0286e87b0bd39d146a0f83aa0de2a2a76d6399
MauroPires <mpiresgro@gmail.com>
2021-03-03 19:46:00 +0000

    Adding category screen

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,24 @@
+import 'package:recipe_book/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CategoryGridScreen extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final mainProviderWatcher = watch(mainProvider);
+
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: GridView.builder(
+        itemCount: mainProviderWatcher.categories.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return GridTile(
+            child: Text("Grid Tile"),
+          );
+        },
+      ),
+    );
+  }
+}
