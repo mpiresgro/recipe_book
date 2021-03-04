760bf5ad35d59d78a512a1eb389867ddb90a3401
MauroPires <mpiresgro@gmail.com>
2021-03-04 12:08:56 +0000

    intial implementation of RecipeListScreen

--- lib/screens/recipe_list_screen.dart
+++ lib/screens/recipe_list_screen.dart
@@ -1,15 +1,12 @@
 import 'package:recipe_book/models/category.dart';
 import 'package:recipe_book/models/recipe.dart';
 import 'package:recipe_book/riverpod/recipe_provider.dart';
-import 'package:recipe_book/main.dart';
-
+// import 'package:recipe_book/screens/add_recipe_screen.dart';
+// import 'package:recipe_book/widgets/recipe_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-enum RecipeFormType {
-  add,
-  edit,
-}
+import 'package:recipe_book/main.dart';
+// import 'package:flutter_slidable/flutter_slidable.dart';
 
 class RecipeListScreen extends ConsumerWidget {
   final int catIndex;
@@ -21,23 +18,58 @@ class RecipeListScreen extends ConsumerWidget {
     final mainProviderWatcher = watch(mainProvider);
     final CategoryModel category = mainProviderWatcher.categories[catIndex];
 
-    // List _buildRecipeList(int count) {
-    //   List<Widget> recipeListItems = List();
+    List _buildRecipeList(int count) {
+      List<Widget> recipeListItems = List();
 
-    //   for (int index = 0; index < count; index++) {
-    //     recipeListItems.add(
-    //       RecipeTile(
-    //         catIndex: catIndex,
-    //         recipeIndex: index,
-    //       ),
-    //     );
-    //   }
-    //   return recipeListItems;
-    // }
+      for (int index = 0; index < count; index++) {
+        recipeListItems.add(
+            Placeholder());
+      }
+      return recipeListItems;
+    }
 
     return Scaffold(
-      appBar: AppBar(title: Text("recipe screen app bar")),
-      body: Text("recipe screen app bar"),
+      backgroundColor: Colors.orangeAccent[50],
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent,
+        toolbarHeight: 100,
+        centerTitle: true,
+        title: Text(
+          category.title,
+          style: TextStyle(fontSize: 40, letterSpacing: 2, wordSpacing: 5),
+        ),
+      ),
+      body: Column(
+        children: category.recipeList == null || category.recipeList.length == 0
+            ? [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 250,
+                  ),
+                  child: Center(
+                    child: Container(
+                      child: Text(
+                        'No recipes in this category',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                )
+              ]
+            : _buildRecipeList(category.recipeList.length),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orangeAccent,
+        foregroundColor: Colors.black,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Placeholder(),
+              ));
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
