b20d1a06ac35897dbffaddd46032aba50afbf122
MauroPires <mpiresgro@gmail.com>
2021-03-10 15:29:42 +0000

    Updated Servings with Consumer

--- lib/widgets/servings.dart
+++ lib/widgets/servings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_picker/flutter_picker.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipe_book/main.dart';
 
 class Servings extends StatefulWidget {
   @override
@@ -11,26 +13,37 @@ class _ServingsState extends State<Servings> {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('Servings $_servingNumber'),
-      onTap: () {
-        showPickerNumber(context);
+    return Consumer(
+      builder: (context, watch, child) {
+        return ListTile(
+          title: Text('Servings $_servingNumber'),
+          onTap: () {
+            showPickerNumber(context, watch);
+          },
+        );
       },
     );
   }
 
-  showPickerNumber(BuildContext context) {
+  showPickerNumber(BuildContext context, ScopedReader watch) {
     Picker(
         changeToFirst: false,
         hideHeader: false,
-        adapter: NumberPickerAdapter(data: [
-          NumberPickerColumn(begin: 0, end: 25, suffix: Icon(Icons.person)),
-        ]),
+        adapter: NumberPickerAdapter(
+          data: [
+            NumberPickerColumn(
+              begin: 0,
+              end: 25,
+              suffix: Icon(Icons.person,),
+            ),
+          ],
+        ),
         title: Text("Serving number"),
         selectedTextStyle: TextStyle(color: Colors.blue),
         onConfirm: (Picker picker, List value) {
           setState(() {
             _servingNumber = value[0];
+            watch(recipeProvider).setServingNumber = _servingNumber;
           });
         }).showModal(context);
   }
