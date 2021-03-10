d5c63da76b98fe526d8b5de2b477685976fc7929
MauroPires <mpiresgro@gmail.com>
2021-03-10 15:57:07 +0000

    Styling

--- lib/widgets/servings.dart
+++ lib/widgets/servings.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_picker/flutter_picker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recipe_book/main.dart';
+import 'package:recipe_book/screens/add_recipe_screen.dart';
 
 class Servings extends StatefulWidget {
   @override
@@ -15,11 +16,30 @@ class _ServingsState extends State<Servings> {
   Widget build(BuildContext context) {
     return Consumer(
       builder: (context, watch, child) {
-        return ListTile(
-          title: Text('Servings $_servingNumber'),
-          onTap: () {
-            showPickerNumber(context, watch);
-          },
+        return Padding(
+          padding: const EdgeInsets.only(left: 30.0, right: 120.0),
+          child: ListTile(
+            title: AddRecipeText(text: 'Servings'),
+            trailing: SizedBox(
+              width: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    ' $_servingNumber',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Icon(
+                    Icons.person,
+                    color: Colors.orange[900],
+                  )
+                ],
+              ),
+            ),
+            onTap: () {
+              showPickerNumber(context, watch);
+            },
+          ),
         );
       },
     );
@@ -27,6 +47,18 @@ class _ServingsState extends State<Servings> {
 
   showPickerNumber(BuildContext context, ScopedReader watch) {
     Picker(
+        cancelTextStyle: TextStyle(
+          color: Colors.orange[900],
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        confirmTextStyle: TextStyle(
+          color: Colors.orange[900],
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        headerColor: Colors.orange[100],
+        backgroundColor: Colors.orange[50],
         changeToFirst: false,
         hideHeader: false,
         adapter: NumberPickerAdapter(
@@ -34,12 +66,20 @@ class _ServingsState extends State<Servings> {
             NumberPickerColumn(
               begin: 0,
               end: 25,
-              suffix: Icon(Icons.person,),
+              suffix: Icon(
+                Icons.person,
+                color: Colors.orange[900],
+              ),
             ),
           ],
         ),
-        title: Text("Serving number"),
-        selectedTextStyle: TextStyle(color: Colors.blue),
+        title: Text(
+          "Serving number",
+          style: TextStyle(color: Colors.orange[900], fontSize: 20),
+        ),
+        selectedTextStyle: TextStyle(
+          color: Colors.orange[900],
+        ),
         onConfirm: (Picker picker, List value) {
           setState(() {
             _servingNumber = value[0];
